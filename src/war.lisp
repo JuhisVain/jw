32c19a1ea4b4fa003ce96f8ca393a830cff0e677
Juho Vainio <Juho.Vainio@student.laurea.fi>
2018-10-15 17:31:21 +0300

    rewrote graphics initialisation

--- src/war.lisp
+++ src/war.lisp
@@ -28,6 +28,11 @@
   (rail-links nil)
   (units nil))
 
+(defstruct graphics
+  (surface nil)
+  (x-at 0)  ; modifiers for drawing
+  (y-at 0))
+
 (defvar *world* nil)
 
 (defun init-test (height width)
@@ -88,16 +93,13 @@
 	    (:key-down-event ()
 			     (sdl:push-quit-event))
 
-	    
 	    (:mouse-motion-event (:x x :y y)
 				 (setf selector-tile
 				       (cursor-coordinates-on-map
 					x y x-shift y-shift))
 				 (setf selector-graphics 
 				       (cursor-coordinates-on-screen
-					x y x-shift y-shift (car selector-tile)))
-				 )
-
+					x y x-shift y-shift (car selector-tile))))
 	    
 	    (:mouse-button-down-event
 	     (:button button :x x :y y)
@@ -167,13 +169,13 @@
       ))
   
   (sdl:draw-surface-at-*
-   (eval selector)
+   (graphics-surface (eval selector))
    (car selector-graphics) (cdr selector-graphics))
   )
 
 (defun draw-tile (x y x-shift y-shift)
   ;; Draw tile's basic type
-  (sdl:draw-surface-at-* (eval (tile-type (aref (world-map *world*) x y)))
+  (sdl:draw-surface-at-* (graphics-surface (eval (tile-type (aref (world-map *world*) x y))))
 			 (+ (* x (car tile-size))
 			    x-shift)
 			 (+ (if (evenp x) (* y (cdr tile-size))
@@ -182,7 +184,7 @@
 
   ;; Draw tile's variant list
   (dolist (variant (tile-variant (aref (world-map *world*) x y)))
-    (sdl:draw-surface-at-* (eval variant)
+    (sdl:draw-surface-at-* (graphics-surface (eval variant))
 			   (+ (* x (car tile-size))
 			      x-shift)
 			   (+ (if (evenp x) (* y (cdr tile-size))
@@ -215,34 +217,85 @@
      ,@body))
 
 
-(defmacro tile-graphics-setup (tile-symbol color-key)
-  `(progn (setf (sdl:color-key-enabled-p ,tile-symbol) t)
-	  (setf (sdl:color-key ,tile-symbol) ,color-key)))
+;;(defmacro tile-graphics-setup (tile-symbol color-key)
+;;  `(progn (setf (sdl:color-key-enabled-p ,tile-symbol) t)
+;;	  (setf (sdl:color-key ,tile-symbol) ,color-key)))
+
+(defmacro tile-graphics-setup (tile-symbol &optional (x-offset 0) (y-offset 0))
+  (let* ((symbol-string (symbol-name tile-symbol))
+	 (to-conc nil))
+    (cond ((search "SOUTH-WEST" symbol-string)
+	   (setf to-conc "SW"))
+	  ((search "SOUTH-EAST" symbol-string)
+	   (setf to-conc "SE"))
+	  ((search "SOUTH" symbol-string)
+	   (setf to-conc "S"))
+	  ((search "NORTH-EAST" symbol-string)
+	   (setf to-conc "NE"))
+	  ((search "NORTH-WEST" symbol-string)
+	   (setf to-conc "NW"))
+	  ((search "NORTH" symbol-string)
+	   (setf to-conc "N")))
+
+    `(prog1
+	 (defparameter ,tile-symbol
+	   (make-graphics :surface
+			  (sdl-image:load-image
+			   (concatenate 'string "graphics/"
+					(subseq (substitute #\_ #\- ,symbol-string) 0
+						(or (search "SOUTH" ,symbol-string)
+						    (search "NORTH" ,symbol-string)))
+					,to-conc
+					".png"))
+			  :x-at ,x-offset :y-at ,y-offset))
+       (setf (sdl:color-key-enabled-p (graphics-surface ,tile-symbol)) t)
+       (setf (sdl:color-key (graphics-surface ,tile-symbol)) (sdl:color :r 255 :g 0 :b 255)))))
 
 (defun load-tiles ()
 
   (let ((color-key (sdl:color :r 255 :g 0 :b 255)))
     
     "Initializes tile graphics"
-    ;;128,104 is tile size -> 102 is distance from right point to lower left point:
+    ;; 128,104 is tile size -> 102 is distance from right point to lower left point:
     (defparameter tile-large-size '(102 . 104))
-    (defparameter sea-large (sdl-image:load-image "graphics/SEA_LARGE.png"))
-    (tile-graphics-setup sea-large color-key)
-    (defparameter grass-large (sdl-image:load-image "graphics/GRASS_LARGE.png"))
-    (tile-graphics-setup grass-large color-key)
-    (defparameter sea-large-border-south (sdl-image:load-image "graphics/SEA_LARGE_BORDER_S.png"))
-    (tile-graphics-setup sea-large-border-south color-key)
-    (defparameter sea-large-border-north (sdl-image:load-image "graphics/SEA_LARGE_BORDER_N.png"))
-    (tile-graphics-setup sea-large-border-north color-key)
-    (defparameter sea-large-border-south-east (sdl-image:load-image "graphics/SEA_LARGE_BORDER_SE.png"))
-    (tile-graphics-setup sea-large-border-south-east color-key)
-    (defparameter sea-large-border-south-west (sdl-image:load-image "graphics/SEA_LARGE_BORDER_SW.png"))
-    (tile-graphics-setup sea-large-border-south-west color-key)
-    (defparameter sea-large-border-north-east (sdl-image:load-image "graphics/SEA_LARGE_BORDER_NE.png"))
-    (tile-graphics-setup sea-large-border-north-east color-key)
-    (defparameter sea-large-border-north-west (sdl-image:load-image "graphics/SEA_LARGE_BORDER_NW.png"))
-    (tile-graphics-setup sea-large-border-north-west color-key)
+    
+;;    (defparameter sea-large (sdl-image:load-image "graphics/SEA_LARGE.png"))
+;;    (tile-graphics-setup sea-large color-key
+;;    (defparameter grass-large (sdl-image:load-image "graphics/GRASS_LARGE.png"))
+;;    (tile-graphics-setup grass-large color-key)
+;;    (defparameter sea-large-border-south (sdl-image:load-image "graphics/SEA_LARGE_BORDER_S.png"))
+;;    (tile-graphics-setup sea-large-border-south color-key)
+;;    (defparameter sea-large-border-north (sdl-image:load-image "graphics/SEA_LARGE_BORDER_N.png"))
+;;    (tile-graphics-setup sea-large-border-north color-key)
+;;    (defparameter sea-large-border-south-east (sdl-image:load-image "graphics/SEA_LARGE_BORDER_SE.png"))
+;;    (tile-graphics-setup sea-large-border-south-east color-key)
+;;    (defparameter sea-large-border-south-west (sdl-image:load-image "graphics/SEA_LARGE_BORDER_SW.png"))
+;;    (tile-graphics-setup sea-large-border-south-west color-key)
+;;    (defparameter sea-large-border-north-east (sdl-image:load-image "graphics/SEA_LARGE_BORDER_NE.png"))
+;;    (tile-graphics-setup sea-large-border-north-east color-key)
+;;    (defparameter sea-large-border-north-west (sdl-image:load-image "graphics/SEA_LARGE_BORDER_NW.png"))
+;;    (tile-graphics-setup sea-large-border-north-west color-key)
 
+    (tile-graphics-setup sea-large)
+    (tile-graphics-setup grass-large)
+    (tile-graphics-setup sea-large-border-south)
+    (tile-graphics-setup sea-large-border-south-west)
+    (tile-graphics-setup sea-large-border-south-east)
+    (tile-graphics-setup sea-large-border-north)
+    (tile-graphics-setup sea-large-border-north-west)
+    (tile-graphics-setup sea-large-border-north-east)
+
+;;    (defparameter stream-large-north-east (sdl-image:load-image "graphics/STREAM_LARGE_NE.png"))
+;;    (tile-graphics-setup stream-large-north-east color-key)
+;;    (defparameter stream-large-south-east (sdl-image:load-image "graphics/STREAM_LARGE_SE.png"))
+;;    (tile-graphics-setup stream-large-south-east color-key)
+;;    (defparameter stream-large-south (sdl-image:load-image "graphics/STREAM_LARGE_S.png"))
+;;    (tile-graphics-setup stream-large-south color-key)
+
+    (tile-graphics-setup stream-large-north-east)
+    (tile-graphics-setup stream-large-south-east)
+    (tile-graphics-setup stream-large-south)
+    
     ;;(62,52) -> 49
     (defparameter tile-small-size '(49 . 52))
     (defparameter sea-small (sdl-image:load-image "graphics/SEA_SMALL.png"))
@@ -250,9 +303,13 @@
     (defparameter grass-small (sdl-image:load-image "graphics/GRASS_SMALL.png"))
     (setf (sdl:alpha-enabled-p grass-small) t)
 
-    (defparameter selector-large (sdl-image:load-image "graphics/SELECT_LARGE.png"))
-    (setf (sdl:color-key-enabled-p selector-large) t)
-    (setf (sdl:color-key selector-large) color-key)
+
+    
+;;    (defparameter selector-large (sdl-image:load-image "graphics/SELECT_LARGE.png"))
+;;    (setf (sdl:color-key-enabled-p selector-large) t)
+    ;;    (setf (sdl:color-key selector-large) color-key)
+
+    (tile-graphics-setup selector-large)
     
     (defparameter selector-small (sdl-image:load-image "graphics/SELECT_SMALL.png"))
     (setf (sdl:alpha-enabled-p selector-small) t)
@@ -277,12 +334,46 @@
 	 (defparameter coast-n sea-large-border-north)
 	 (defparameter coast-ne sea-large-border-north-east)
 	 (defparameter coast-nw sea-large-border-north-west)
+
+	 (defparameter stream-ne stream-large-north-east)
+	 (defparameter stream-se stream-large-south-east)
+	 (defparameter stream-s stream-large-south)
 	 )
 	((equal var 'small)
 	 (defparameter selector selector-small)
 	 (defparameter tile-size tile-small-size)
 	 (defparameter sea sea-small)
 	 (defparameter grass grass-small))))
+
+(defun add-river (tile-x tile-y size direction &optional (recursion t))
+  (let ((river-symbol
+	 (intern
+	  (concatenate 'string
+		       (string-upcase (symbol-name size)) "-"
+		       (string-upcase (symbol-name direction))))))
+    ;; Logic rivers
+    (pushnew river-symbol
+	     (tile-river-borders (aref (world-map *world*) tile-x tile-y)))
+    ;; Graphic rivers
+    (if (member direction '(S SE NE))
+	(pushnew river-symbol
+		 (tile-variant (aref (world-map *world*) tile-x tile-y))))
+
+    (if recursion
+	(let ((opposing
+	       (neighbour-tile-coords tile-x tile-y direction *world*)))
+	  (add-river (car opposing) (cdr opposing) size
+		     (cond ((eq direction 'N) 'S)
+			   ((eq direction 'NE) 'SW)
+			   ((eq direction 'NW) 'SE)
+			   ((eq direction 'S) 'N)
+			   ((eq direction 'SE) 'NW)
+			   ((eq direction 'SW) 'NE))
+		     nil)))))
+  
+
+(defun create-rivers ()
+  )
 
 (defun init-world (width height)
   (let ((world (make-world)))
@@ -294,7 +385,7 @@
     (do ((x 0)
 	 (y 0))
 	((>= x (array-dimension (world-map world) 0)))
-      (setf (aref (world-map world) x y) (make-tile :type (if (= (random 2) 1) 'sea 'grass)))
+      (setf (aref (world-map world) x y) (make-tile :type (if (< (random 4) 1) 'sea 'grass)))
       (incf y)
       (if (>= y (array-dimension (world-map world) 1))
 	  (progn (incf x)
