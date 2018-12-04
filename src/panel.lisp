73480ecdab8a4005966219246895ad8f67072e61
Juho Vainio <Juho.Vainio@student.laurea.fi>
2018-12-04 14:48:22 +0200

    refactoring UI into OO

--- src/panel.lisp
+++ src/panel.lisp
@@ -15,30 +15,121 @@
   (height nil)
   (elements nil))
 
-(defstruct panel-button
+(defstruct OBSOLETEpanel-button
   (position nil)
   (width nil)
   (height nil)
   (icon nil)
   (action nil))
 
+(defstruct OBSOLETEpanel-list
+  (position nil)
+  (width nil)
+  (height nil)
+  (display nil)
+  (element-height)
+  (action nil))
+
+(defclass panel-button ()
+  ((x
+    :accessor panel-element-x
+    :initarg :x)
+   (y
+    :accessor panel-element-y
+    :initarg :y)
+   (width
+    :accessor panel-element-width
+    :initarg :width)
+   (height
+    :accessor panel-element-height
+    :initarg :height)
+   (icon
+    :accessor panel-button-icon
+    :initarg :icon)
+   (action
+    :accessor panel-button-action
+    :initarg :action)
+   (parent
+    :accessor panel-button-parent
+    :initarg :parent)
+   ))
+
+(defmethod display ((button panel-button))
+  (sdl:draw-surface-at-* (panel-button-icon button)
+			 (+ (panel-element-x button) (panel-x (panel-button-parent button)))
+			 (+ (panel-element-y button) (panel-y (panel-button-parent button)))
+			 :surface window))
+
+(defmethod click ((button panel-button) ) ;; Might wanna use mouse state later
+  (funcall (panel-button-action button)))
+
+(defclass panel-list ()
+  ((x
+    :accessor panel-element-x
+    :initarg :x)
+   (y
+    :accessor panel-element-y
+    :initarg :y)
+   (width
+    :accessor panel-element-width
+    :initarg :width)
+   (height
+    :accessor panel-element-height
+    :initarg :height)
+   (element-height
+    :accessor panel-list-element-height
+    :initarg :element-height)
+   (element-width
+    :accessor panel-list-element-width
+    :initarg :element-width)
+   (action
+    :accessor panel-list-action
+    :initarg :action)))
+
+(defmethod display ((list panel-list))
+  ())
+
+(defmacro create-panel (x y width height &rest elements)
+  ;; Sorting the elements from greatest to smallest
+  ;;   -> will end up in panel slot smallest to greatest
+  (let ((sorted-elements
+	 (sort elements #'(lambda (one two)
+			    (and (> (getf one :y) (getf two :y)) ;; oh la la
+				 (> (getf one :x) (getf two :x))
+				 t)))))
+    `(let ((panel (make-panel :x ,x :y ,y
+			      :width ,width :height ,height
+			      :elements nil)))
+       (dolist (element ',sorted-elements)
+	 (nconc element (list :parent panel))
+	 (push (eval element) (panel-elements panel)))
+    panel))) ;; panel panel panel PANEL PANEL PANEL
+
 (defun setup-panels ()
   (setf *panel-setup* nil)
   ;; TODO: pushnew seems to work here for now butbutbut...
   ;; also will need to do testing with sdl-image:load-image
-  (pushnew (make-panel :x (- (sdl:width window) 200) :y 0
-		       :width 200 :height (sdl:height window)
-		       :elements (list
-				  (make-panel-button
-				   :position (cons 4 4)
-				   :width 48 :height 48
-				   :icon (sdl-image:load-image "graphics/PANEL_BUTTON_TEST.png")
-				   :action #'(lambda () (format t "First button~%")))
-				  (make-panel-button
-				   :position (cons 56 4)
-				   :width 48 :height 48
-				   :icon (sdl-image:load-image "graphics/PANEL_BUTTON_TEST.png")
-				   :action #'(lambda () (format t "Second button~%")))))
+  (pushnew (create-panel (- (sdl:width window) 200)
+			 0
+			 200
+			 (sdl:height window)
+			 (make-instance 'panel-button
+					:x 4 :y 4
+					:width 48 :height 48
+					:icon (sdl-image:load-image "graphics/PANEL_BUTTON_TEST.png")
+					:action #'(lambda () (format t "First button~%")))
+			 (make-instance 'panel-button
+					:x 56 :y 4
+					:width 48 :height 48
+					:icon (sdl-image:load-image "graphics/PANEL_BUTTON_TEST.png")
+					:action #'(lambda () (format t "Second button~%"))) )
+  ;;				  (make-panel-list
+;;				   :position (cons 4 56)
+;;				   :width 192
+;;				   :height 450
+;;				   :display 
+;;				   :element-height 50
+;;				   :action)))
 	   *panel-setup*))
 
 (defun draw-panels ()
@@ -56,12 +147,7 @@
 		 :surface window :color sdl:*red*)
 
 		(mapcar #'(lambda (element)
-				   (sdl:draw-surface-at-* (panel-button-icon element)
-							  (+ (panel-x panel)
-							     (car (panel-button-position element)))
-							  (+ (panel-y panel)
-							     (cdr (panel-button-position element)))
-							  :surface window))
+				   (display element))
 			(panel-elements panel)))
      *panel-setup*))
 
@@ -81,45 +167,51 @@
   (draw-unit-list selected-tile selected-unit))
 
 
-(defun draw-unit-list (selected-tile selected-unit)
-  (if (or (null selected-tile) (< (car selected-tile) 0) (< (cdr selected-tile) 0))
-      (return-from draw-unit-list))
-  (do* ((unit-list (reverse (tile-units (tile-at
-					 (car selected-tile) (cdr selected-tile))))
-		   (cdr unit-list))
-	(unit (car unit-list) (car unit-list))
-	(unit-shift 0 (+ unit-shift *panel-list-height*)))
-       ((or (null unit) (>= unit-shift *panel-unitlist-height*)))
+(defun draw-unit-list (selected-tile selected-unit panel-list)
+  (let ((panel-element-height (panel-list-height panel-list))
+	(panel-list-element-height (panel-list-element-height panel-list))
+	(panel-list-width (panel-list-width panel-list)))
+    (if (or (null selected-tile) (< (car selected-tile) 0) (< (cdr selected-tile) 0))
+	(return-from draw-unit-list))
+    (do* ((unit-list (reverse (tile-units (tile-at
+					   (car selected-tile) (cdr selected-tile))))
+		     (cdr unit-list))
+	  (unit (car unit-list) (car unit-list))
+	  (unit-shift 0 (+ unit-shift panel-list-element-height)))
+	 ((or (null unit) (>= unit-shift panel-list-height)))
 
-    (if (eq unit selected-unit)
-	(sdl:draw-rectangle-* (- (sdl:width window) *panel-width*) (+ 0 unit-shift)
-			      100 *panel-list-height* :surface window :color sdl:*black*))
-    
-    (sdl:draw-surface-at-* (graphics-surface (army-counter unit))
-			   (- (sdl:width window) *panel-width*)
-			   (+ 0 unit-shift)
-			   :surface window)
+      (if (eq unit selected-unit) ; indicating selected unit
+	  (sdl:draw-rectangle-* (panel-list-x panel-list)
+				(+ (panel-list-y panel-list) unit-shift)
+				(panel-list-element-width panel-list)
+				(panel-list-element-height panel-list)
+				:surface window :color sdl:*black*))
+      
+      (sdl:draw-surface-at-* (graphics-surface (army-counter unit))
+			     (- (sdl:width window) *panel-width*)
+			     (+ 0 unit-shift)
+			     :surface window)
 
-    (sdl:draw-string-solid-* (write-to-string (car (army-troops unit)))
-			     (+ (- (sdl:width window) *panel-width*) 100)
-			     (+ 25 unit-shift)
-			     :surface window :color sdl:*white*)
-    ))
+      (sdl:draw-string-solid-* (write-to-string (car (army-troops unit)))
+			       (+ (- (sdl:width window) *panel-width*) 100)
+			       (+ 25 unit-shift)
+			       :surface window :color sdl:*white*)
+      )))
 
 (defun click-panel (mouse-button mouse-button-state mouse-x mouse-y
 		    &optional (panel (car *panel-setup*)))
   (let ((relative-x (- mouse-x (panel-x panel)))
 	(relative-y (- mouse-y (panel-y panel))))
     (dolist (element (panel-elements panel))
-      (cond ((panel-button-p element)
+      (cond ((typep element 'panel-button)
 	     (and ; Depending on the alignment of panel these could be optimized to some other order
-	      (> relative-x (car (panel-button-position element)))
-	      (<= relative-x (+ (car (panel-button-position element))
-				(panel-button-width element)))
-	      (> relative-y (cdr (panel-button-position element)))
-	      (<= relative-y (+ (cdr (panel-button-position element))
-				(panel-button-height element)))
-		(funcall (panel-button-action element))))))))
+	      (> relative-x (panel-element-x element))
+	      (<= relative-x (+ (panel-element-x element)
+				(panel-element-width element)))
+	      (> relative-y (panel-element-y element))
+	      (<= relative-y (+ (panel-element-y element)
+				(panel-element-height element)))
+	      (funcall (panel-button-action element))))))))
 
 (defun select-from-panel (mouse-y selected-tile)
   (nth (floor mouse-y *panel-list-height*) (reverse (tile-units selected-tile))))
