f07b40f1cac40b78712f640105b51c4472965f19
Juho Vainio <Juho.Vainio@student.laurea.fi>
2018-12-23 12:07:29 +0200

    Free chopper's utility surfaces

--- src/tilechopper.lisp
+++ src/tilechopper.lisp
@@ -53,87 +53,92 @@
       (let ((chopping-block
 	     (sdl:create-surface tile-width tile-height)))
 
-	;; Behold!
-	(mapcar #'(lambda (coord)
-		    
-		    (sdl:fill-surface *war-color-key* :surface chopping-block)
-		    (draw-at (car coord) (cdr coord) 0 0 raw-tile-image chopping-block)
-		    (sdl:draw-surface template :surface chopping-block)
+	(prog1 ;; Return result of mapcar, not free
+	    ;; Behold!
+	    (mapcar #'(lambda (coord)
+			
+			(sdl:fill-surface *war-color-key* :surface chopping-block)
+			(draw-at (car coord) (cdr coord) 0 0 raw-tile-image chopping-block)
+			(sdl:draw-surface template :surface chopping-block)
 
-		    ;; Determine bounds of actual graphics:
-		    (let ((upper-bound)
-			  (lower-bound)
-			  (left-bound)
-			  (right-bound))
+			;; Determine bounds of actual graphics:
+			(let ((upper-bound)
+			      (lower-bound)
+			      (left-bound)
+			      (right-bound))
 
-		      (and (setf upper-bound
-				 (do ((x 0) (y 0))
-				     ((>= y tile-height) nil) ; return nil if no graphics found -> ends (and)
-				   (if (not (sdl:color= (sdl:read-pixel-* x y :surface chopping-block)
-							*war-color-key*))
-				       (return y))
-				   (and (incf x)
-					(>= x tile-width)
-					(setf x 0)
-					(incf y))))
-			   (setf lower-bound
-				 (do ((x 0) (y (1- tile-height)))
-				     ((< y 0) nil)
-				   (if (not (sdl:color= (sdl:read-pixel-* x y :surface chopping-block)
-							*war-color-key*))
-				       (return y))
-				   (and (incf x)
-					(>= x tile-width)
-					(setf x 0)
-					(decf y))))
-			   (setf left-bound
-				 (do ((x 0) (y 0))
-				     ((>= x tile-width) nil)
-				   (if (not (sdl:color= (sdl:read-pixel-* x y :surface chopping-block)
-							*war-color-key*))
-				       (return x))
-				   (and (incf y)
-					(>= y tile-height)
-					(setf y 0)
-					(incf x))))
-			   (setf right-bound
-				 (do ((x (1- tile-width)) (y 0))
-				     ((< x 0) nil)
-				   (if (not (sdl:color= (sdl:read-pixel-* x y :surface chopping-block)
-							*war-color-key*))
-				       (return x))
-				   (and (incf y)
-					(>= y tile-height)
-					(setf y 0)
-					(decf x)))))
-		      ;; Check'em:
-		      (if (not (and upper-bound lower-bound left-bound right-bound))
-			  nil ; return nil to resulting list in case of 'empty' graphics
+			  (and (setf upper-bound
+				     (do ((x 0) (y 0))
+					 ((>= y tile-height) nil) ; return nil if no graphics found -> ends (and)
+				       (if (not (sdl:color= (sdl:read-pixel-* x y :surface chopping-block)
+							    *war-color-key*))
+					   (return y))
+				       (and (incf x)
+					    (>= x tile-width)
+					    (setf x 0)
+					    (incf y))))
+			       (setf lower-bound
+				     (do ((x 0) (y (1- tile-height)))
+					 ((< y 0) nil)
+				       (if (not (sdl:color= (sdl:read-pixel-* x y :surface chopping-block)
+							    *war-color-key*))
+					   (return y))
+				       (and (incf x)
+					    (>= x tile-width)
+					    (setf x 0)
+					    (decf y))))
+			       (setf left-bound
+				     (do ((x 0) (y 0))
+					 ((>= x tile-width) nil)
+				       (if (not (sdl:color= (sdl:read-pixel-* x y :surface chopping-block)
+							    *war-color-key*))
+					   (return x))
+				       (and (incf y)
+					    (>= y tile-height)
+					    (setf y 0)
+					    (incf x))))
+			       (setf right-bound
+				     (do ((x (1- tile-width)) (y 0))
+					 ((< x 0) nil)
+				       (if (not (sdl:color= (sdl:read-pixel-* x y :surface chopping-block)
+							    *war-color-key*))
+					   (return x))
+				       (and (incf y)
+					    (>= y tile-height)
+					    (setf y 0)
+					    (decf x)))))
+			  ;; Check'em:
+			  (if (not (and upper-bound lower-bound left-bound right-bound))
+			      nil ; return nil to resulting list in case of 'empty' graphics
 
-			  ;; Create image of proper size to hold the chopped graphics
-			  (let* ((final-width (1+ (- right-bound left-bound)))
-				 (final-height (1+ (- lower-bound upper-bound)))
-				 (final-image
-				  (sdl:create-surface final-width
-						      final-height)))
+			      ;; Create image of proper size to hold the chopped graphics
+			      (let* ((final-width (1+ (- right-bound left-bound)))
+				     (final-height (1+ (- lower-bound upper-bound)))
+				     (final-image
+				      (sdl:create-surface final-width
+							  final-height)))
 
-			    (sdl:draw-surface-at-* chopping-block (- left-bound) (- upper-bound) :surface final-image)
-			    (setf (sdl:color-key-enabled-p final-image) t)
-			    (setf (sdl:color-key final-image) *war-color-key*)
+				(sdl:draw-surface-at-* chopping-block (- left-bound) (- upper-bound) :surface final-image)
+				(setf (sdl:color-key-enabled-p final-image) t)
+				(setf (sdl:color-key final-image) *war-color-key*)
 
-			    ;;(format t "~&~a , x-at: ~a , y-at: ~a~%"
-			;;	    coord (- tile-width final-width left-bound) (- tile-height final-height upper-bound))
+				;;(format t "~&~a , x-at: ~a , y-at: ~a~%"
+				;;	    coord (- tile-width final-width left-bound) (- tile-height final-height upper-bound))
 
-			    (make-graphics :surface final-image
-					   :x-at left-bound
-					   :y-at upper-bound)))))
+				(make-graphics :surface final-image
+					       :x-at left-bound
+					       :y-at upper-bound)))))
 
-		;; mapcar's list:
-		(list (cons 0 0)       ;center
-		      (cons 0 -1)      ;north from center -> south of it's own tile
-		      (cons 1 -1)      ;north-east           south-west
-		      (cons 1 0)       ;south-east           north-west
-		      (cons 0 1)       ;south                north
-		      (cons -1 0)      ;south-west           north-east
-		      (cons -1 -1))))));north-west           south-east
-	  
+		    ;; mapcar's list:
+		    (list (cons 0 0)       ;center
+			  (cons 0 -1)      ;north from center -> south of it's own tile
+			  (cons 1 -1)      ;north-east           south-west
+			  (cons 1 0)       ;south-east           north-west
+			  (cons 0 1)       ;south                north
+			  (cons -1 0)      ;south-west           north-east
+			  (cons -1 -1)))   ;north-west           south-east
+
+	  (sdl:free (graphics-surface raw-tile-image))
+	  (sdl:free template)
+	  (sdl:free chopping-block)))))
+
