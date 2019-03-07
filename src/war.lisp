27d662ed4320131481899eb8cee1214dab9484cf
Juho Vainio <Juho.Vainio@student.laurea.fi>
2019-03-07 14:18:28 +0200

    Remove obsolete create-city func

--- src/war.lisp
+++ src/war.lisp
@@ -95,13 +95,6 @@
   (setf *world* (init-world height width :algo 'testing :mirror mirror :islands islands))
   nil)
 
-;; This is mostly for testing. do same for 'suburb-a'
-;; The right and lower side tiles will draw themselves over this one.
-;; TODO: either do a second pass on tiles or split city graphics kinda like the sea/coasts
-(defun OBSOLETEcreate-city (x y &optional (world *world*))
-  (pushnew 'city (tile-location (aref (world-map world) x y)))
-  (pushnew 'city-a (tile-variant (aref (world-map world) x y))))
-
 (defun create-location-at (location-symbol x y &optional (world *world*))
   ;;(pushnew location-symbol (tile-location (aref (world-map world) x y)))
   (if (eq location-symbol 'city) (pushnew location-symbol (tile-location (aref (world-map world) x y))))
@@ -111,7 +104,6 @@
   (pushnew (intern (concatenate 'string (symbol-name location-symbol) "-A")) ;; WIP
 	   (tile-variant (aref (world-map world) x y)))
   (finalize-tile-region x y world))
-
 
 
 (defun cursor-coordinates-on-map (screen-x screen-y x-shift y-shift)
