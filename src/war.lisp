858ed89d71d91b63f2bbb6d54663630037f57098
Juho Vainio <Juho.Vainio@student.laurea.fi>
2019-04-01 09:30:08 +0300

    Quote out old load-tiles & set-tile-size

--- src/war.lisp
+++ src/war.lisp
@@ -587,7 +587,7 @@
 
     
 
-(defun load-tiles ()
+'(defun OBSOLETEload-tiles ()
   ;;(tile-graphics-setup sea-large 100 -4 -9)
   (create-tile sea :large (100 -4 -9) :small (0 0 0))
   (create-tile grass :large (0 0 0) :small (0 0 0))
@@ -620,7 +620,7 @@
   (set-tile-size 'large)
   )
 
-(defun set-tile-size (var)
+'(defun OBSOLETEset-tile-size (var)
   "Switches between tile sizes"
   (sdl:clear-display sdl:*black*)
 
