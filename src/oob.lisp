32f8f3c7c51ec624be8a20e12ecb1446d549a873
JuhisVain <Juho.Vainio@student.laurea.fi>
2019-11-03 10:48:37 +0200

    Printer funcs for oob-elements

--- src/oob.lisp
+++ src/oob.lisp
@@ -8,19 +8,41 @@
 (defstruct oob-element
   (army nil :type (or null army)))
 
-(defstruct (supreme-hq (:include oob-element))
+(defstruct (supreme-hq (:include oob-element)
+		       (:print-object supreme-hq-printer))
   (general nil :type (or general null))
   (subordinates nil)
   (supply-sources nil))
 
-(defstruct (sub-hq (:include oob-element))
+(defstruct (sub-hq (:include oob-element)
+		   (:print-object sub-hq-printer))
   (general nil :type (or general null))
   (superior nil :type (or sub-hq supreme-hq))
   (subordinates nil)
   (supply-sources nil))
 
-(defstruct (oob-pos (:include oob-element))
+(defstruct (oob-pos (:include oob-element)
+		    (:print-object oob-pos-printer))
   (superior nil :type (or sub-hq supreme-hq)))
+
+(defun oob-pos-printer (this stream)
+  (declare (oob-pos this))
+  (format stream "~&Subordinate of ~a~%" (oob-pos-superior this)))
+
+(defun sub-hq-printer (this stream)
+  (declare (sub-hq this))
+  (format
+   stream
+   "Sub-HQ of general ~a~%with ~a subordinates~%Commanding-HQ ~a~%"
+   (sub-hq-general this)
+   (length (sub-hq-subordinates this))
+   (sub-hq-superior this)))
+
+(defun supreme-hq-printer (this stream)
+  (declare (supreme-hq this))
+  (format stream "Supreme-HQ of general ~a~%with ~a subordinates~%"
+	  (supreme-hq-general this)
+	  (length (supreme-hq-subordinates this))))
 
 (defvar *turn-readiness-replenishment* 25)
 
